6562e54ece13e99676fa1845c03a2a6c239cff3d
Chirag <chiraggoyalbcs@gmail.com>
2025-11-16 18:38:10 +0530

    refactor: simplify block and check functions for better readability in main single file

--- interface-array.c
+++ interface-array.c
@@ -130,22 +130,9 @@ int pin(char z)
 // }
 void block(int x,int y,int dx,int dy)
 {
-    if(dx==1)
-    {
-        {
-            state[x+1][y]=state[x+1][y+dy/2]=1;
-        }
-    }
-    else if(dx==-1)
-    {
-        x--;
-        state[x+1][y]=state[x+1][y+dy/2]=1;
-    }
-    else if(dx==2)
-    {
-        if(dy==1)state[x+dx/2][y]=state[x][y]=1;
-    }
-    else state[x+dx/2][y]=state[x][y]=1;
+    if(abs(dx)==1)state[x+(dx>0)][y+(dy>0)]=state[x+(dx>0)][y+(dy>0)+dy/2]=1;
+    else if(abs(dx)==2)state[x+(dx>0)-(dx<0)][y+(dy>0)]=state[x+(dx>0)*dx][y+(dy>0)]=1;
+    return;
 }
 bool check(int x,int y,int dx,int dy)
 {
